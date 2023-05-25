3acfb55f748fb14fa60fde67725d6252b084cb98
Anju Mathew <anju@sayonetech.com>
2023-05-25 18:01:37 +0530

    category get and events add

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -16,14 +16,25 @@ import {
 import Button from "@/components/button/Button";
 import { Select } from "@chakra-ui/react";
 import { db } from "@/libs/firebase";
+import CustomInput from "@/components/input/Input";
 
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+} from "firebase/firestore";
 
 function HomePageCommon() {
   const [userDataArray, setUserDataArray] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
+  const [eventName, setEventName] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
 
   useEffect(() => {
     let unsubscribed = false;
@@ -31,7 +42,7 @@ function HomePageCommon() {
     getDocs(collection(db, "categories"))
       .then((querySnapshot) => {
         if (unsubscribed) return; // unsubscribed? do nothing.
-
+        console.log(querySnapshot, "querySnapshot");
         const newUserDataArray = querySnapshot.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
@@ -49,6 +60,15 @@ function HomePageCommon() {
     return () => (unsubscribed = true);
   }, []);
   console.log(userDataArray, "userDataArray");
+
+  const onEventSaveHandler = async (event) => {
+    console.log(event, "event");
+    await setDoc(doc(db, "events"), {
+      eventName: eventName,
+      title: title,
+      price: price,
+    });
+  };
   return (
     <div>
       <h1>Categories Listing Page</h1>
@@ -69,12 +89,26 @@ function HomePageCommon() {
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Event Name</FormLabel>
-              <Input placeholder="Event Name" />
+              <CustomInput
+                type="text"
+                name="eventName"
+                value={eventName}
+                onChange={(event) => setEventName(event.target.value)}
+                id="eventName"
+                placeholder="Event Name"
+              />
             </FormControl>
 
             <FormControl mt={4}>
               <FormLabel>Title</FormLabel>
-              <Input placeholder="Last name" />
+              <CustomInput
+                type="text"
+                name="title"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                id="title"
+                placeholder="Event Title"
+              />
             </FormControl>
             <FormControl mt={4}>
               <FormLabel>Date</FormLabel>
@@ -82,7 +116,14 @@ function HomePageCommon() {
             </FormControl>
             <FormControl mt={4}>
               <FormLabel>Price</FormLabel>
-              <Input placeholder="Price" />
+              <CustomInput
+                type="text"
+                name="price"
+                value={price}
+                onChange={(event) => setPrice(event.target.value)}
+                id="price"
+                placeholder="Event Price"
+              />
             </FormControl>
 
             <FormControl mt={4}>
@@ -97,7 +138,12 @@ function HomePageCommon() {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} title="Save" />
+            <Button
+              colorScheme="blue"
+              mr={3}
+              title="Save"
+              onClick={onEventSaveHandler()}
+            />
             <Button rounded="lg" onClick={onClose} title="Cancel" />
           </ModalFooter>
         </ModalContent>
