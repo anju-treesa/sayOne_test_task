e2520d2a6b68521d460b52a86eeba42f135ac594
Anju Mathew <anju@sayonetech.com>
2023-05-25 12:56:12 +0530

    Add events page started

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Modal,
   useDisclosure,
@@ -14,17 +14,46 @@ import {
 } from "@chakra-ui/react";
 
 import Button from "@/components/button/Button";
+import { Select } from "@chakra-ui/react";
+import { db } from "@/libs/firebase";
+
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 function HomePageCommon() {
+  const [userDataArray, setUserDataArray] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
 
+  useEffect(() => {
+    let unsubscribed = false;
+
+    getDocs(collection(db, "categories"))
+      .then((querySnapshot) => {
+        if (unsubscribed) return; // unsubscribed? do nothing.
+
+        const newUserDataArray = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        setUserDataArray(newUserDataArray);
+      })
+      .catch((err) => {
+        if (unsubscribed) return; // unsubscribed? do nothing.
+
+        // TODO: Handle errors
+        console.error("Failed to retrieve data", err);
+      });
+
+    return () => (unsubscribed = true);
+  }, []);
+  console.log(userDataArray, "userDataArray");
   return (
     <div>
       <h1>Categories Listing Page</h1>
 
-      <Button title="Add Category" onSubmit={onOpen} />
+      <Button title="Add Events" onSubmit={onOpen} />
 
       <Modal
         initialFocusRef={initialRef}
@@ -35,17 +64,35 @@ function HomePageCommon() {
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create your account</ModalHeader>
+          <ModalHeader>Create Events</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <FormControl>
-              <FormLabel>First name</FormLabel>
-              <Input placeholder="First name" />
+              <FormLabel>Event Name</FormLabel>
+              <Input placeholder="Event Name" />
             </FormControl>
 
             <FormControl mt={4}>
-              <FormLabel>Last name</FormLabel>
+              <FormLabel>Title</FormLabel>
               <Input placeholder="Last name" />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Date</FormLabel>
+              <Input placeholder="Date" />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Price</FormLabel>
+              <Input placeholder="Price" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Select Category</FormLabel>
+              <Select
+                bg="red"
+                borderColor="black"
+                color="white"
+                placeholder="Select Category"
+              />
             </FormControl>
           </ModalBody>
 
